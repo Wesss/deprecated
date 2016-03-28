e13085e60b4f9ac093d842ca33d17860d945215e
Wesley Cox <Wesley Cox>
2016-03-27 17:14:29 -0700

    updated to fit Overhead refactoring of MainLoop

--- src/maze/Main.java
+++ src/maze/Main.java
@@ -2,9 +2,16 @@ package maze;
 
 import overhead.Overhead;
 
+/**
+ * Kicks off the Maze game
+ * 
+ * @author Wesley Cox
+ * @last_edited 3/27/16
+ */
 public class Main {
+	
 	public static void main(String args[]) {
-		Overhead.startGame(MazeGame.class, MazeGame.GAME_AREA);
+		Overhead.startGame(MazeGame.class, MazeGame.FPS, MazeGame.GAME_AREA);
 		while (true) {}
 	}
 }
--- src/maze/MazeGame.java
+++ src/maze/MazeGame.java
@@ -1,46 +1,39 @@
 package maze;
 
 import java.awt.Dimension;
-
 import overhead.MainLoop;
 import overhead_interfaces.Game;
 
+/**
+ * This class represents the Maze Game itself
+ * 
+ * @author Wesley Cox
+ * @last_edited 3/27/16
+ */
 public class MazeGame implements Game {
 	
 	public static final Dimension GAME_AREA = new Dimension(600, 600);
+	public static final int FPS = 60;
 	
 	private Maze maze;
 
-	public MazeGame() {
+	public MazeGame(MainLoop mainLoop) {
 		maze = new Maze(200, 200, Maze.Type.CYCLELESS);
-		MainLoop.add(maze, 0);
+		mainLoop.add(maze, 0);
 	}
 	
 	@Override
-	public void keyPressed(int keyCode) {
-		// TODO Auto-generated method stub
-	}
+	public void keyPressed(int keyCode) {}
 
 	@Override
-	public void keyReleased(int keyCode) {
-		// TODO Auto-generated method stub
-
-	}
+	public void keyReleased(int keyCode) {}
 
 	@Override
-	public void mousePressed(int x, int y, int button) {
-		// TODO Auto-generated method stub
-
-	}
+	public void mousePressed(int x, int y, int button) {}
 
 	@Override
-	public void mouseReleased(int x, int y, int button) {
-		// TODO Auto-generated method stub
-
-	}
+	public void mouseReleased(int x, int y, int button) {}
 
 	@Override
-	public void mouseMoved(int x, int y) {
-		// TODO Auto-generated method stub
-	}
+	public void mouseMoved(int x, int y) {}
 }
