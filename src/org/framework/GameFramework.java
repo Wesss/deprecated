fb2f3e72f8dc7ddeb15c0c964b8271052109aab8
Wesley Cox <Wesley Cox>
2016-06-13 22:30:54 -0700

    rewrote startGame

--- src/org/framework/GameFramework.java
+++ src/org/framework/GameFramework.java
@@ -19,7 +19,6 @@ public class GameFramework {
 	/**
 	 * Bugs
 	 * TODO BUG1: Some rare race condition still exists (via dodger DodgerGame startup)
-	 * TODO BUG2: Sometimes keyboard listener does not work upon startup (via EventFireTest)
 	 */
 	
 	/**
@@ -36,45 +35,45 @@ public class GameFramework {
 	 * 
 	 * @return the MainLoop created for this game
 	 */
-	public static <T extends GameEventListener> MainLoop startGame(Class<T> listener, int fps, Dimension dimension) {
-		MainLoop mainLoop = MainLoop.init(fps);
-		GamePanel panel = new GamePanel(dimension);
-		
-		GameEventListener game = null;
-		
-		try {
-			Constructor<?> emptyConstructor = null;
-			
-			Constructor<?>[] constructors = listener.getConstructors();
-			for (int i = 0; i < constructors.length; i++) {
-				Class<?>[] parameters = constructors[i].getParameterTypes();
-				if (parameters.length == 1 && parameters[0].equals(MainLoop.class)) {
-					game = (GameEventListener)constructors[i].newInstance(mainLoop);
-				} else if (parameters.length == 0) {
-					emptyConstructor = constructors[i];
-				}
-			}
-			
-			if (game == null) {
-				if (emptyConstructor != null) {
-					game = (GameEventListener)emptyConstructor.newInstance();
-				} else {
-					throw new RuntimeException("given game class does not contain an appropriate constructor");
-				}
-			}
-		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
-				| InvocationTargetException e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
-		
-		mainLoop.setReferences(panel);
-		panel.setReferences(game, mainLoop);
-		
-		mainLoop.start();
-		
-		return mainLoop;
-	}
+//	public static <T extends GameEventListener> MainLoop startGame(Class<T> listener, int fps, Dimension dimension) {
+//		MainLoop mainLoop = MainLoop.init(fps);
+//		GamePanel panel = new GamePanel(dimension);
+//		
+//		GameEventListener game = null;
+//		
+//		try {
+//			Constructor<?> emptyConstructor = null;
+//			
+//			Constructor<?>[] constructors = listener.getConstructors();
+//			for (int i = 0; i < constructors.length; i++) {
+//				Class<?>[] parameters = constructors[i].getParameterTypes();
+//				if (parameters.length == 1 && parameters[0].equals(MainLoop.class)) {
+//					game = (GameEventListener)constructors[i].newInstance(mainLoop);
+//				} else if (parameters.length == 0) {
+//					emptyConstructor = constructors[i];
+//				}
+//			}
+//			
+//			if (game == null) {
+//				if (emptyConstructor != null) {
+//					game = (GameEventListener)emptyConstructor.newInstance();
+//				} else {
+//					throw new RuntimeException("given game class does not contain an appropriate constructor");
+//				}
+//			}
+//		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+//				| InvocationTargetException e) {
+//			e.printStackTrace();
+//			System.exit(1);
+//		}
+//		
+//		mainLoop.setReferences(panel);
+//		panel.setReferences(game, mainLoop);
+//		
+//		mainLoop.start();
+//		
+//		return mainLoop;
+//	}
 	
 	/**
 	 * TODO
@@ -84,10 +83,40 @@ public class GameFramework {
 	 * @param updatesPerSecond
 	 * @return
 	 */
-	public static <T extends Game> MainLoop startGame(Class<T> game, GameEventListener listener,
+	public static <T extends Game> MainLoop startGame(Class<T> game,
+														GameEventListener listener,
 														AspectRatio ratio,
 														int updatesPerSecond) {
-		// TODO
+		MainLoop mainLoop = MainLoop.init(updatesPerSecond);
+		GamePanel panel = new GamePanel(ratio);
+		
+		Game newGame = null;
+		
+		try {
+			Constructor<?> emptyConstructor = null;
+			
+			Constructor<?>[] constructors = game.getConstructors();
+			for (int i = 0; i < constructors.length; i++) {
+				Class<?>[] parameters = constructors[i].getParameterTypes();
+				if (parameters.length == 0) {
+					emptyConstructor = constructors[i];
+				}
+			}
+			
+			if (emptyConstructor != null) {
+				newGame = (Game)emptyConstructor.newInstance();
+			} else {
+				throw new RuntimeException("given game class does not contain an empty constructor");
+			}
+		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+				| InvocationTargetException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		
+		mainLoop.setReferences(panel);
+		panel.setReferences(newGame, mainLoop);
+		mainLoop.start();
 		return null;
 	}
 	
