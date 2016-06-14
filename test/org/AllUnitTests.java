36a197c8ad2f2b099d00279ccfba6350fd90481d
Wesley Cox <Wesley Cox>
2016-06-13 23:18:49 -0700

    made framework test stub

--- test/org/AllUnitTests.java
+++ test/org/AllUnitTests.java
@@ -3,10 +3,11 @@ package org;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
+import org.unit.GameFrameworkTest;
 import org.unit.MainLoopTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ MainLoopTest.class })
+@SuiteClasses({ GameFrameworkTest.class, MainLoopTest.class })
 public class AllUnitTests {
 
 }
