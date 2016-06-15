6401ece350b70cde7d2be0efad7c9586cac2e2c2
Wesley Cox <Wesley Cox>
2016-06-14 21:28:55 -0700

    unit test helper

--- test/org/unit/MainLoopTest.java
+++ test/org/unit/MainLoopTest.java
@@ -1,14 +1,13 @@
 package org.unit;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import org.framework.MainLoop;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 public class MainLoopTest{
 	
@@ -16,6 +15,27 @@ public class MainLoopTest{
 	public static final int SLOW_UPS = 2;
 	public static final int STANDARD_UPS = 60;
 	public static final int STRESSTEST_UPS = 300;
+	
+	//////////////////////////////////////////////////
+	// Helpers
+	//////////////////////////////////////////////////
+	
+	private MainLoop initMainLoop(int UPS) {
+		MainLoop mainloop = null;
+		try {
+			Method init = MainLoop.class.getDeclaredMethod("init", int.class);
+			init.setAccessible(true);
+			mainloop = (MainLoop) init.invoke(null, STANDARD_UPS);
+		} catch (SecurityException | IllegalArgumentException | NoSuchMethodException
+				| IllegalAccessException | InvocationTargetException e) {
+			fail();
+		}
+		return mainloop;
+	}
+	
+	//////////////////////////////////////////////////
+	// Tests
+	//////////////////////////////////////////////////
 	
 	// @Rule
     // public ExpectedException thrown = ExpectedException.none();
@@ -49,7 +69,7 @@ public class MainLoopTest{
 				IllegalArgumentException e) {
 			fail();
 		} catch (InvocationTargetException e) {
-			// pass!
+			// pass
 		}
 	}
 
