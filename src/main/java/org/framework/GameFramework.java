66ae6b41f81a7209e72956bbad25de1ef5e6fadb
Wesley Cox <wesleymcox@gmail.com>
2016-08-25 20:22:27 -0700

    startGame now call exactly one constructor of given game

--- src/main/java/org/framework/GameFramework.java
+++ src/main/java/org/framework/GameFramework.java
@@ -26,7 +26,6 @@ import static java.lang.Math.min;
 public class GameFramework {
 
     // TODO make game require a factory method to allow giving of mainloop and canvas
-    // TODO prevent misuse of public framework functions (ie set references, start?)
 
     public static final GameEventListener<Game> EMPTY_GAME_LISTENER = new EmptyGameListener();
     public static final double SCREEN_RATIO = 0.9;
@@ -34,6 +33,9 @@ public class GameFramework {
     private GameFramework() {}
 
     /**
+     * Instantiates an instance of the given game class. The class must contain an accessible empty constructor or a
+     * constructor that takes a MainLoop and a GameCanvas.
+     *
      * @param game the class of the game to be run
      *             If a constructor that takes a MainLoop and GameCanvasModel is present, that constructor will be called; passing in
      *             the appropriate game controllers.
@@ -41,13 +43,14 @@ public class GameFramework {
      *             Throws a RuntimeException if neither constructor is present
      * @param listener The listener that will receive user events
      * @param updatesPerSecond
-     * @return a pair where pair.left is the MainLoop and pair.right is the GameCanvasModel to control the game
-     * @throws RuntimeException if a valid constructor for the given game class is not present
+     * @return a pair where pair.key is the MainLoop and pair.value is the GameCanvasModel to control the game
+     * @throws InstantiationException if a valid constructor for the given game class is not present
      * @param <T> the type of the game to run
      */
     public static <T extends Game> Pair<MainLoop, GameCanvas> startGame(Class<T> game,
-                                                                             GameEventListener<? super T> listener,
-                                                                             int updatesPerSecond) {
+                                                                        GameEventListener<? super T> listener,
+                                                                        int updatesPerSecond)
+            throws InstantiationException {
         MainLoopFactory factory = MainLoopFactoryFactory.getMainLoopFactory();
         factory.constructMainLoop(updatesPerSecond);
         MainLoopModel mainLoopModel = factory.getMainLoopModel();
@@ -69,15 +72,16 @@ public class GameFramework {
         return new Pair<>(mainLoop, canvas);
     }
 
-    // TODO instantiate prefered constructor only once
-    private static <T extends Game> T createGame(Class<T> gameClass, MainLoop mainLoop, GameCanvas canvas) {
+    private static <T extends Game> T createGame(Class<T> gameClass, MainLoop mainLoop, GameCanvas canvas)
+            throws InstantiationException{
         T game = null;
         try {
+            Constructor<?> emptyConstructor = null;
             Constructor<?>[] constructors = gameClass.getConstructors();
             for (Constructor<?> constructor : constructors) {
                 Class<?>[] parameters = constructor.getParameterTypes();
                 if (parameters.length == 0) {
-                    game = (T)constructor.newInstance();
+                    emptyConstructor = constructor;
                 } else if (parameters.length == 2) {
                     if (parameters[0].equals(MainLoop.class) && parameters[1].equals(GameCanvas.class)) {
                         game = (T)constructor.newInstance(mainLoop, canvas);
@@ -88,13 +92,16 @@ public class GameFramework {
                     }
                 }
             }
-            if (game == null) {
-                throw new RuntimeException("given game class does not contain an empty or mainloop accepting constructor");
+            if (game == null && emptyConstructor != null) {
+                game = (T)emptyConstructor.newInstance();
             }
         } catch (InstantiationException | IllegalAccessException | IllegalArgumentException
                 | InvocationTargetException e) {
-            //TODO error more gracefully? (ie. with custom exception)
-            throw new RuntimeException(e);
+            throw new InstantiationException(e.getMessage());
+        }
+        if (game == null) {
+            throw new InstantiationException(
+                    "given game class does not contain an empty or MainLoop and GameCanvas accepting constructor");
         }
         return game;
     }
--- src/main/java/org/framework/interfaces/Game.java
+++ src/main/java/org/framework/interfaces/Game.java
@@ -1,9 +1,7 @@
 package org.framework.interfaces;
 
 /**
- * This represents the game itself to be run
- * 
- * @author Wesley Cox
+ * This class represents the game itself to be run
  */
 public interface Game {
     // Nothing! the implementation of the game is entirely up to the client
