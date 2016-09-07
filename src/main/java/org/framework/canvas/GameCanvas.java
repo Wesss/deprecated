5bcb21e27b85646cc8c63f8fc58d87e498ef2b82
wesley <wesleymcox@gmail.com>
2016-09-06 23:50:56 -0700

    created gameCanvas to encompass canvas model and controller

---
+++ src/main/java/org/framework/canvas/GameCanvas.java
@@ -0,0 +1,19 @@
+package org.framework.canvas;
+
+public class GameCanvas {
+    private GameCanvasController controller;
+    private GameCanvasModel model;
+
+    protected GameCanvas(GameCanvasController controller, GameCanvasModel model) {
+        this.controller = controller;
+        this.model = model;
+    }
+
+    public GameCanvasController getController() {
+        return controller;
+    }
+
+    public GameCanvasModel getModel() {
+        return model;
+    }
+}
