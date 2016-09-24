5c77fd3c9916e05e77ea1a555e64a0f9e46708c7
wesley <wesleymcox@gmail.com>
2016-09-23 18:27:29 -1000

    setters for Button, removed EventAcceptor

--- src/main/java/org/gameUtil/Button.java
+++ src/main/java/org/gameUtil/Button.java
@@ -32,16 +32,32 @@ public abstract class Button implements GameObj {
         return x;
     }
 
+    public void setX(int x) {
+        this.x = x;
+    }
+
     public int getY() {
         return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
     }
 
     public int getWidth() {
         return width;
     }
 
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
     public int getHeight() {
         return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
     }
 
     public boolean isCurrentSelection() {
---
+++ src/main/java/org/gameUtil/TimedEvent.java
@@ -0,0 +1,4 @@
+package org.gameUtil;
+
+public class TimedEvent {
+}
