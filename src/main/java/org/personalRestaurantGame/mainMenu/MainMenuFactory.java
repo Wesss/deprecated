50b2d06e28730db7c9964960adc73fda4cd42bbd
wesley <wesleymcox@gmail.com>
2016-09-27 09:21:54 -1000

    completely abstracted out newGameButton and exitButton into button

--- src/main/java/org/personalRestaurantGame/mainMenu/MainMenuFactory.java
+++ src/main/java/org/personalRestaurantGame/mainMenu/MainMenuFactory.java
@@ -1,21 +1,29 @@
 package org.personalRestaurantGame.mainMenu;
 
 import org.framework.mainLoop.MainLoopCustomGroupsInterface;
+import org.gameUtil.Button;
 import org.gameUtil.ButtonList;
 import org.gameUtil.CountdownEvent;
 import org.personalRestaurantGame.RestaurantGame;
 
+import static org.personalRestaurantGame.RestaurantGame.State.*;
 import static org.personalRestaurantGame.mainMenu.MainMenu.*;
 
 public class MainMenuFactory {
 
     public static MainMenu getMainMenu(RestaurantGame game, MainLoopCustomGroupsInterface mainLoop) {
-        ButtonList buttons = new ButtonList(
-                new NewGameButton(game, X, Y_TOP, BUTTON_WIDTH, BUTTON_HEIGHT),
-                new ExitButton(new CountdownEvent(60, game::exit), X, Y_TOP + BUTTON_HEIGHT + Y_MARGIN, BUTTON_WIDTH, BUTTON_HEIGHT));
+
+        Button newGameButton = new Button(
+                new CountdownEvent(60, () -> game.swapState(NEW_GAME)),
+                X, Y_TOP, BUTTON_WIDTH, BUTTON_HEIGHT,
+                "New Game");
+        Button quitButton = new Button(
+                new CountdownEvent(60, () -> game.swapState(NEW_GAME)),
+                X, Y_TOP + BUTTON_HEIGHT + Y_MARGIN, BUTTON_WIDTH, BUTTON_HEIGHT,
+                "Quit");
+
+        ButtonList buttons = new ButtonList(newGameButton, quitButton);
         mainLoop.createGroup(0, 0).add(buttons);
         return new MainMenu(buttons);
     }
-
-
 }
